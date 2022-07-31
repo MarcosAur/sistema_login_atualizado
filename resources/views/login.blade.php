ccc7c226174d498bfcae349d53353329f04fe113
MarcosAur <marcosaureliomendesoliveira@gmail.com>
2022-07-31 15:11:51 -0300

    Sistema de login integrado

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -22,13 +22,13 @@
     
     <div class="d-flex flex-column">
       <h4 class="text-center w-100">Fazer Login</h4>
-       <form class="m-auto">
-
+       <form class="m-auto" method="POST" action='{{route("validarLogin")}}'>
+        @csrf
         <label for="email">Email:</label>
-        <input type="email" class="textInput my-2" name="email" placeholder="seuEmail@3esolucoes.com.br" />
+        <input type="email" class="textInput my-2" name="email" placeholder="seu.email@3esolucoes.com.br" />
         
         <label for="password">Senha:</label>
-        <input type="password" class="textInput my-2" name="password" placeholder="seuEmail@3esolucoes.com.br" />
+        <input type="password" class="textInput my-2" name="senha" placeholder="Sua senha" />
         
         <div class="d-flex justify-content-center flex-column align-items-center mt-5">
           <a href="#" class="simpleLink">Esqueci minha senha.</a>
